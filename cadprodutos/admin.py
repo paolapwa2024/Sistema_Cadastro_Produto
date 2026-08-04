ef22682c4a5179dcef4df31cad3b4e18019b2974
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-08-03 22:44:40 -0300

    upadate_admin
    correção

--- cadprodutos/admin.py
+++ cadprodutos/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Brand, Category, Product
 
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'brand', 'category', 'price',
+                    'is_active', 'created_at', 'updated_at')
+    search_fields = ('title', 'brand__name', 'category__name')
+    list_filter = ('is_active', 'brand', 'category')
