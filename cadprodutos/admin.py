5389a7d81582b32e7a2ae5d960085903876a69d2
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-08-03 21:47:04 -0300

    admin_config

--- cadprodutos/admin.py
+++ cadprodutos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Brand,Category,Product
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name','is_active', 'description','created_at','update_at')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','is_active','description',)
